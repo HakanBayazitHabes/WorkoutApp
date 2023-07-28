f9b1ebc791a41e8bc5d7d8af79b25622b448401b
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-07-28 15:11:29 +0300

    Creating our own custom recyclerviewadapter and viewholder

---
+++ app/src/main/java/com/hakanbayazithabes/workoutapp/ExercisestatusAdapter.kt
@@ -0,0 +1,35 @@
+package com.hakanbayazithabes.workoutapp
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.hakanbayazithabes.workoutapp.databinding.ActivityExerciseBinding
+import com.hakanbayazithabes.workoutapp.databinding.ItemExerciseStatusBinding
+
+class ExerciseStatusAdapter(val items: ArrayList<ExerciseModel>) :
+    RecyclerView.Adapter<ExerciseStatusAdapter.ViewHolder>() {
+
+    class ViewHolder(binding: ItemExerciseStatusBinding) : RecyclerView.ViewHolder(binding.root) {
+        val tvItem = binding.tvItem
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemExerciseStatusBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val model: ExerciseModel = items[position]
+        holder.tvItem.text = model.getId().toString()
+    }
+
+}
