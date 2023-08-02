e698a285b70ef9c5343a8d3ecc3cff1951cf5b3c
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-08-02 20:57:33 +0300

    Fetching Data from our Database and displaying it in the log

--- app/src/main/java/com/hakanbayazithabes/workoutapp/HistoryActivity.kt
+++ app/src/main/java/com/hakanbayazithabes/workoutapp/HistoryActivity.kt
@@ -2,7 +2,11 @@ package com.hakanbayazithabes.workoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import com.hakanbayazithabes.workoutapp.databinding.ActivityHistoryBinding
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class HistoryActivity : AppCompatActivity() {
     private var binding: ActivityHistoryBinding? = null
@@ -21,5 +25,23 @@ class HistoryActivity : AppCompatActivity() {
         binding?.toolbarHistoryActivity?.setNavigationOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
+
+        val dao = (application as WorkOutApp).db?.historyDao()
+        dao?.let { getAllCompletedDates(it) }
+    }
+
+    private fun getAllCompletedDates(historyDao: HistoryDao) {
+        lifecycleScope.launch {
+            historyDao.fetchAllDates().collect() { allCompeletedDatesList ->
+                for (i in allCompeletedDatesList) {
+                    Log.e("Date: ", "" + i.date)
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
