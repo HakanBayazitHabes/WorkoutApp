7c1bfe4499130537e9377ac273a0c2e1b77018e9
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-08-03 00:07:26 +0300

    Displaying the Workout History on our Recyclerview

--- app/src/main/java/com/hakanbayazithabes/workoutapp/HistoryActivity.kt
+++ app/src/main/java/com/hakanbayazithabes/workoutapp/HistoryActivity.kt
@@ -3,7 +3,9 @@ package com.hakanbayazithabes.workoutapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.hakanbayazithabes.workoutapp.databinding.ActivityHistoryBinding
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -33,8 +35,24 @@ class HistoryActivity : AppCompatActivity() {
     private fun getAllCompletedDates(historyDao: HistoryDao) {
         lifecycleScope.launch {
             historyDao.fetchAllDates().collect() { allCompeletedDatesList ->
-                for (i in allCompeletedDatesList) {
-                    Log.e("Date: ", "" + i.date)
+                if (allCompeletedDatesList.isNotEmpty()) {
+                    binding?.tvHistory?.visibility = View.VISIBLE
+                    binding?.rvHistory?.visibility = View.VISIBLE
+                    binding?.tvNoDataAvailable?.visibility = View.INVISIBLE
+
+                    binding?.rvHistory?.layoutManager = LinearLayoutManager(this@HistoryActivity)
+
+                    val dates = ArrayList<String>()
+                    for (date in allCompeletedDatesList) {
+                        dates.add(date.date)
+                    }
+                    val historyAdapter = HistoryAdapter(dates)
+                    binding?.rvHistory?.adapter = historyAdapter
+
+                } else {
+                    binding?.tvHistory?.visibility = View.GONE
+                    binding?.rvHistory?.visibility = View.GONE
+                    binding?.tvNoDataAvailable?.visibility = View.VISIBLE
                 }
             }
         }
